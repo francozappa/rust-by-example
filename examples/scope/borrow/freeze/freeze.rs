1505192328ac40e1e2ac2fbf451e531b57bc612e
francozappa <antonioli.daniele@gmail.com>
2016-10-06 08:01:03 +0800

    More emphasis on the mutable owner

--- examples/scope/borrow/freeze/freeze.rs
+++ examples/scope/borrow/freeze/freeze.rs
@@ -1,17 +1,31 @@
 fn main() {
-    let mut _mutable_integer = 7i32;
+    let mut _mutable_integer = 7_i32;
 
     {
         // Borrow `_mutable_integer`
         let _large_integer = &_mutable_integer;
 
         // Error! `_mutable_integer` is frozen in this scope
-        _mutable_integer = 50;
+        _mutable_integer = 50_i32;
         // FIXME ^ Comment out this line
 
         // `_large_integer` goes out of scope
     }
 
+    {
+        // Mutable Borrow `_mutable_integer`
+        let _large_integer = &mut _mutable_integer;
+
+        // Error! `_mutable_integer` is frozen in this scope
+        _mutable_integer = 50_i32;
+        // FIXME ^ Comment out this line
+
+        // Ok! Using dereferencing
+        *_large_integer = 6_i32;
+
+        // `_large_integer` goes out of scope
+    }
+
     // Ok! `_mutable_integer` is not frozen in this scope
-    _mutable_integer = 3;
+    _mutable_integer = 3_i32;
 }
--- examples/scope/borrow/freeze/input.md
+++ examples/scope/borrow/freeze/input.md
@@ -1,4 +1,6 @@
-When data is immutably borrowed, it also *freezes*. *Frozen* data can't be 
-modified via the original object until all references to it go out of scope:
+When mutable data is borrowed, its owner *freezes*. Borrowed data can't be
+modified via the original owner until all references to it go out of scope,
+however it can be modified by a mutable reference, using the dereference
+operator:
 
 {freeze.play}
