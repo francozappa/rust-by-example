6780162f1c4294bf5738606b1d48893431dcdf1c
francozappa <antonioli.daniele@gmail.com>
2016-10-07 09:03:04 +0800

    Rename num_lifetime to num

--- examples/scope/lifetime/static_lifetime/static_lifetime.rs
+++ examples/scope/lifetime/static_lifetime/static_lifetime.rs
@@ -1,7 +1,7 @@
 // Make a constant with `'static` lifetime.
 static NUM: i32 = 18;
 
-// Returns a reference to `NUM` where its `'static` 
+// Returns a reference to `NUM` where its `'static`
 // lifetime is coerced to that of the input argument.
 fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
     &NUM
@@ -16,16 +16,16 @@ fn main() {
         // When `static_string` goes out of scope, the reference
         // can no longer be used, but the data remains in the binary.
     }
-    
+
     {
         // Make an integer to use for `coerce_static`:
-        let lifetime_num = 9;
+        let num = 9;
 
-        // Coerce `NUM` to lifetime of `lifetime_num`:
-        let coerced_static = coerce_static(&lifetime_num);
+        // Coerce `NUM` to lifetime of `num`:
+        let coerced_static = coerce_static(&num);
 
         println!("coerced_static: {}", coerced_static);
     }
-    
+
     println!("NUM: {} stays accessible!", NUM);
 }
