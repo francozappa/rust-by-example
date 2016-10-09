d50818237c4485ca1da80d576c3bd27ee40c2cb5
francozappa <antonioli.daniele@gmail.com>
2016-10-10 07:43:07 +0800

    Better descriptions

--- examples/trait/clone/clone.rs
+++ examples/trait/clone/clone.rs
@@ -1,40 +1,41 @@
-// A unit struct without resources
+// A unit struct without members
 #[derive(Debug, Clone, Copy)]
 struct Nil;
 
-// A tuple struct with resources that implements the `Clone` trait
+// A tuple struct with members that implements the `Clone` trait
 #[derive(Clone, Debug)]
 struct Pair(Box<i32>, Box<i32>);
 
 fn main() {
     // Instantiate `Nil`
     let nil = Nil;
-    // Copy `Nil`, there are no resources to move
+    // Copy `Nil`, there are no members to move
     let copied_nil = nil;
 
     // Both `Nil`s can be used independently
-    println!("original: {:?}", nil);
-    println!("copy: {:?}", copied_nil);
+    println!("nil: {:?}", nil);
+    println!("copied_nil: {:?}", copied_nil);
 
     // Instantiate `Pair`
     let pair = Pair(Box::new(1), Box::new(2));
-    println!("original: {:?}", pair);
+    println!("pair: {:?}", pair);
 
-    // Copy `pair` into `moved_pair`, moves resources
+    // Bind `pair` to `moved_pair` (move semantics)
     let moved_pair = pair;
-    println!("copy: {:?}", moved_pair);
+    println!("moved_pair: {:?}", moved_pair);
 
-    // Error! `pair` has lost its resources
-    //println!("original: {:?}", pair);
+    // Error! `use of a moved value: pair`
+    //println!("pair: {:?}", pair);
     // TODO ^ Try uncommenting this line
-    
-    // Clone `moved_pair` into `cloned_pair` (resources are included)
+
+    // Clone `moved_pair`, and bind the clone to `cloned_pair`
     let cloned_pair = moved_pair.clone();
-    // Drop the original pair using std::mem::drop
+
+    // Drop `moved_pair` and its members
     drop(moved_pair);
 
-    // Error! `moved_pair` has been dropped
-    //println!("copy: {:?}", moved_pair);
+    // Error! `use of a moved value: moved_pair`
+    //println!("moved_pair: {:?}", moved_pair);
     // TODO ^ Try uncommenting this line
 
     // The result from .clone() can still be used!
